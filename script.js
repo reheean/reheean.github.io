37e04f3e78f5bffa077d8dcfaa5e04ae20504e9d
Rodoshie Reheean <rodoshiereheean@gmail.com>
2025-04-22 21:46:54 +0600

    Initial commit with my academic website files

---
+++ script.js
@@ -0,0 +1,5 @@
+// Add a simple interactivity to the "Learn More" button
+document.getElementById('learn-more').addEventListener('click', function() {
+    alert('Thank you for your interest! More content will be coming soon.');
+  });
+  
